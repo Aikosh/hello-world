9670cf5cdcfd4fe4d12e7bfa5de9f43a08d54522
Aikosh <t5617002@iwate-u.ac.jp>
2018-07-27 16:25:27 +0900

    incdec.cxx

---
+++ incdec.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+
+main( ) {
+    using namespace std;
+
+    int i = 0;
+    int j = 0;
+
+    cout << "i++ " << i ++ << " ++j " << ++ j << endl;
+    cout << "i-- " << i -- << " --j " << -- j << endl;
+    cout << "i   " << i    << "   j " << j << endl;
+
+}
